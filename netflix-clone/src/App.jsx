cef2e17ef558940bed19de496944d95fd4d5e7b9
mohammed-abDev <mohammedabdudev11@gmail.com>
2025-11-27 17:50:48 +0300

    feat: add header and footer component

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 import './App.css'
+
+import Home from './pages/Home/Home'
 
 function App() {
  
 
   return (
     <>
-      <h1>hellow world</h1>
+      <Home/>
     </>
   )
 }
---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../componentes/Header/Header'
+import Footer from '../../componentes/Footer/Footer'
+
+function Home() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
